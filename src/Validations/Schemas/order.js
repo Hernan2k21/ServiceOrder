0cd5d96f9ad200724cd91c4e1889805ad34f5da7
Hernan2k21 <hernan@genosha.com.ar>
2022-09-05 15:48:12 -0300

    update product in order validation schema

--- src/Validations/Schemas/order.js
+++ src/Validations/Schemas/order.js
@@ -2,7 +2,8 @@ const Joi = require("joi");
 
 const productSchema = Joi.object().keys({
   productId: Joi.number().required(),
-  price: Joi.number().required()
+  price: Joi.number().required(),
+  quantity: Joi.number().required()
 })
 
 const  orderSchema = Joi.object().keys({
